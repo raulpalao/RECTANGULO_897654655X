c3dd1159f8fff811fa307b36ad140331b0e87e37
raulpalao <raul.palao@ceedcv.es>
2022-01-13 19:59:37 +0100

    Se ha modificado Rectangulo

--- src/Rectangulo.java
+++ src/Rectangulo.java
@@ -1,13 +1,35 @@
+/**
+ * Clase rectángulo con cuatro lados
+ * @author raulpalao
+ * @version 1.0
+ */
 public class Rectangulo {
 
     private int lado1;
     private int lado2;
     
+    /**
+     * Calcula el perímetro del rectángulo
+     * @return El perímetro del rectángulo
+     */
     public int perimetro(){
         return lado1 + lado1 + lado2 + lado2;
     }
     
+    /**
+     * Calcula el área del rectángulo
+     * @return El área del rectángulo
+     */
     public int area(){
         return lado1 * lado2;
+    }
+    
+    /**
+     * Escalar el rectángulo
+     * @param factor Valor a multiplicar
+     */
+    public void multiplicar(int factor){
+        int name = lado1 * factor;
+        int name2 = lado2 * factor;
     }
 }
